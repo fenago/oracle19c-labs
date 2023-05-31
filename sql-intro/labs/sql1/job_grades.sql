43d180d8b6553fc61888aff38e875111ffcd3dca
athertahir <athertahir1995@gmail.com>
2023-05-31 09:03:46 +0500

    added job_grades.sql

---
+++ sql-intro/labs/sql1/job_grades.sql
@@ -0,0 +1,17 @@
+CREATE TABLE job_grades (
+grade 		CHAR(1),
+lowest_sal 	NUMBER(8,2) NOT NULL,
+highest_sal	NUMBER(8,2) NOT NULL
+);
+
+ALTER TABLE job_grades
+ADD CONSTRAINT jobgrades_grade_pk PRIMARY KEY (grade);
+
+INSERT INTO job_grades VALUES ('A', 1000, 2999);
+INSERT INTO job_grades VALUES ('B', 3000, 5999);
+INSERT INTO job_grades VALUES ('C', 6000, 9999);
+INSERT INTO job_grades VALUES ('D', 10000, 14999);
+INSERT INTO job_grades VALUES ('E', 15000, 24999);
+INSERT INTO job_grades VALUES ('F', 25000, 40000);
+
+COMMIT;
