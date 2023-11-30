8489c87143f76997ff2a3ba4da94af4e174891f4
athertahir <athertahir1995@gmail.com>
2023-11-30 22:58:12 +0500

    updated

--- backup-recovery/labs/DBMod_Backup/DBMod_Backup_workload.sh
+++ backup-recovery/labs/DBMod_Backup/DBMod_Backup_workload.sh
@@ -12,12 +12,11 @@
 
 sqlplus / as sysdba<<EOF
 ALTER SESSION set container=orclpdb1;
-CONNECT inventory/pass4INV@orclpdb1
 /* retrieve a random integer between 1 an 100 use that as a mod function */
 /* to change the obsolete date for product ids in the table */
 
 
-update product_master set obsoleted = SYSDATE
+update inventory.product_master set obsoleted = SYSDATE
 where mod(product_id,100) IN (
   select (round(dbms_random.value(1,100))) from dual)
 /
