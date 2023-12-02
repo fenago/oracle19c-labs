0d31531fdc800ba671665fbc175b8645a0eaedf6
athertahir <athertahir1995@gmail.com>
2023-12-02 19:44:56 +0500

    updated

--- backup-recovery/labs/DBMod_Flashback/check_02_03.sql
+++ backup-recovery/labs/DBMod_Flashback/check_02_03.sql
@@ -3,6 +3,7 @@
 -- NOT supported by Oracle World Wide Technical Support.
 -- The script has been tested and appears to work as intended.
 -- You should always run new scripts on a test instance initially
+ALTER SESSION set container=orclpdb1;
 
 select e.department_id, department_name, COUNT(*)
 from bar.barcopy E, bar.bardept D
